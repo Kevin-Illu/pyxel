36d2e4edf712c345e26407739152f74b68ec4932
kevin-illu <kevinillu97@gmail.com>
2024-08-11 21:45:33 -0600

    feat(tilemap): playing with the tilemap

--- src/main.py
+++ src/main.py
@@ -4,18 +4,32 @@ import pyxel
 class App:
     def __init__(self) -> None:
         pyxel.init(160, 120)
-        self.x = 0
+
+        # load the tilemap resource
+        pyxel.load("./my_resource.pyxres")
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
-        self.x = (self.x + 1) % pyxel.width
-
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+
+        # Draw a single tile at the center of the screen
+        # The parameters are:
+        # 1. Screen X and Y coordinates (in pixels)
+        screen_x, screen_y = 0, 0
+        # 2. Tilemap layer
+        tilemap_layer = 0
+        # 3. Tilemap X and Y coordinates (in tiles)
+        tilemap_x, tilemap_y = 0, 0
+        # 4. Width and Height of the tile area to draw (in tiles)
+        width, height = 8, 8
+        pyxel.bltm(
+            screen_x, screen_y, tilemap_layer, tilemap_x, tilemap_y, width, height
+        )
 
 
 App()
